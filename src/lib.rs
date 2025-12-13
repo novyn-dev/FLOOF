1ad18ee6c198c57c15f9cf2e30fab012f35010da
novyn-dev <zairysbigtae@gmail.com>
2025-12-13 19:18:25 +0800

    lib.rs'

---
+++ src/lib.rs
@@ -0,0 +1,79 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+/// combines both println! and serial_println!
+macro_rules! log {
+    ($($arg:tt)*) => {{
+        println!($($arg)*);
+        serial_println!($($arg)*);
+    }};
+}
+
+
+pub trait Testable {
+    fn run(&self);
+}
+
+impl<T: Fn()> Testable for T {
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Testable]) {
+    log!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]");
+    serial_println!("Error: {}", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+
+pub mod vga_buffer;
+pub mod serial;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4); // 0xf4 is the port to exit
+        port.write(code as u32);
+    }
+}
+
+#[cfg(test)]
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() {
+    test_main();
+    loop {}
+}
+
+#[test_case]
+fn it_works() {
+    let sum = 1 + 1;
+    assert_eq!(sum, 2);
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,21 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-#![reexport_test_harness_main = "test_main"]
-mod vga_buffer;
-mod serial;
 
+#[allow(unused_imports)]
 use core::panic::PanicInfo;
-use crate::vga_buffer::{Color, vga_color};
+use floof::{print, println};
+use floof::vga_buffer::{Color, vga_color};
 
-/// combines both println! and serial_println!
-macro_rules! log {
-    ($($arg:tt)*) => {{
-        println!($($arg)*);
-        serial_println!($($arg)*);
-    }};
-}
+#[allow(clippy::empty_loop)]
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    print!("Make yourself at home - ");
+    vga_color(Color::Yellow, Color::Black);
+    println!("Novyn");
+    vga_color(Color::White, Color::Black);
 
-#[allow(dead_code)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4); // 0xf4 is the port to exit
-        port.write(code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self);
-}
-
-impl<T: Fn()> Testable for T {
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    log!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
+    loop {}
 }
 
 #[cfg(not(test))]
@@ -65,28 +28,6 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]");
-    serial_println!("Error: {}", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[allow(clippy::empty_loop)]
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    print!("Make yourself at home - ");
-    vga_color(Color::Yellow, Color::Black);
-    println!("Novyn");
-    vga_color(Color::White, Color::Black);
-
-    #[cfg(test)]
-    test_main();
-
-    loop {}
-}
-
-#[test_case]
-fn it_works() {
-    let sum = 1 + 1;
-    assert_eq!(sum, 2);
+    use floof::test_panic_handler;
+    test_panic_handler(info);
 }
