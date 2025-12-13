b61ffec1ceb47b0aef0c5a5ecb0e5f75e27e64dd
novyn-dev <32233160-novyn@users.noreply.gitlab.com>
2025-12-13 12:44:08 +0800

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 mod vga_buffer;
 
 use core::panic::PanicInfo;
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
