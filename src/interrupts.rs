6928e82d001cb53b6421cacd9cf6d5e6b80acb0f
novyn-dev <zairysbigtae@gmail.com>
2025-12-14 15:57:05 +0800

    different msg

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -7,5 +7,5 @@ pub fn init_idt() {
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
-    println!("wo ai ni");
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
