30594fc176c9c2e9c5754eb956e6b9aa204e91b0
novyn-dev <zairysbigtae@gmail.com>
2025-12-18 16:45:17 +0800

    was messing around

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,12 @@
 #[allow(unused_imports)]
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
-use floof::{QemuExitCode, Testable, exit_qemu, print, println, serial_println};
+use floof::memory::{BootInfoFrameAllocator, EmptyFrameAllocator};
+use floof::{QemuExitCode, Testable, exit_qemu, memory, print, println, serial_println};
 use floof::vga_buffer::{Color, vga_color};
+use x86_64::VirtAddr;
 use x86_64::registers::control::Cr3;
+use x86_64::structures::paging::{FrameAllocator, Page, PageTable, Translate};
 
 macro_rules! log {
     ($($arg:tt)*) => {{
@@ -35,6 +38,34 @@ fn kernel_entry(boot_info: &'static BootInfo) -> ! {
     vga_color(Color::White, Color::Black);
 
     floof::init();
+
+    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    // let mut frame_allocator = unsafe {
+    //     BootInfoFrameAllocator::init(&boot_info.memory_map)
+    // };
+    //
+    // let page = Page::containing_address(VirtAddr::new(0));
+    // memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    //
+    // let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    // unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) }
+    // let addrs = [
+    //     // the identity-mapped vga buffer page
+    //     0xb8000,
+    //     // some code page
+    //     0x201008,
+    //     // some stack page
+    //     0x0100_0020_1a10,
+    //     // virtual address mapped to physical address 0
+    //     boot_info.physical_memory_offset,
+    // ];
+    //
+    // for &addr in &addrs {
+    //     let virt = VirtAddr::new(addr);
+    //     let phys = mapper.translate_addr(virt);
+    //     println!("{virt:?} -> {phys:?}");
+    // }
 
     #[cfg(test)]
     test_main();
--- src/memory.rs
+++ src/memory.rs
@@ -1,12 +1,75 @@
-use x86_64::{VirtAddr, structures::paging::PageTable};
+use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
+use x86_64::{PhysAddr, VirtAddr, registers::control::Cr3, structures::paging::{FrameAllocator, Mapper, OffsetPageTable, Page, PageTable, PageTableFlags, PhysFrame, Size4KiB}};
 
-pub unsafe fn active_level4_table(offset: VirtAddr) -> &'static mut PageTable {
-    use x86_64::registers::control::Cr3;
-    let (l4_table, _) = Cr3::read();
+pub struct EmptyFrameAllocator;
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        None
+    }
+}
 
-    let phys = l4_table.start_address();
+/// A FrameAllocator that returns usable frames from the bootloader's memory map.
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize
+}
+
+impl BootInfoFrameAllocator {
+    /// Create a FrameAllocator from the passed memory map.
+    ///
+    /// This function is unsafe because the caller must guarantee that the passed
+    /// memory map is valid. The main requirement is that all frames that are marked
+    /// as `USABLE` in it are really unused.
+    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+        Self {
+            memory_map,
+            next: 0,
+        }
+    }
+
+    pub fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+        let regions = self.memory_map.iter();
+        let usable_regions = regions.filter(|r| r.region_type == MemoryRegionType::Usable);
+        let addr_ranges = usable_regions.map(|r| r.range.start_frame_number..r.range.end_frame_number);
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
+    }
+}
+
+pub unsafe fn init(offset: VirtAddr) -> OffsetPageTable<'static> {
+    unsafe {
+        let level_4_table = active_level4_table(offset);
+        OffsetPageTable::new(level_4_table, offset)
+    }
+}
+
+/// MUST BE CALLED ONLY ONCE
+unsafe fn active_level4_table(offset: VirtAddr) -> &'static mut PageTable {
+    let (l4_frame, _) = Cr3::read();
+    let phys = l4_frame.start_address();
     let virt = offset + phys.as_u64();
-    let table_ptr: *mut PageTable = virt.as_mut_ptr();
+    let table: *mut PageTable = virt.as_mut_ptr();
 
-    unsafe { &mut *table_ptr }
+    unsafe { &mut *table }
+}
+
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000)); // vga
+    let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+    let res = unsafe {
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    res.expect("map_to failed").flush();
 }
